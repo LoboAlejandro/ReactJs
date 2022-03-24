332382e81250653e3bd69d6445361031241cbd65
alejandro lobo <alejandro.lobo_97@hotmail.com>
2022-03-24 16:48:44 -0300

    Correccion Carrito v2

--- src/components/Header/Carrito.js
+++ src/components/Header/Carrito.js
@@ -22,26 +22,26 @@ export default function Carrito() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-return (
-    <div className='imgCarrito'>
-        <Button onClick={handleOpen}>
-            <ShoppingCartIcon className="iconoCarrito" fontSize="large"/>
-        </Button>
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            >
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Modal de prueba
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Acá irian objetos
-                </Typography>
-            </Box>
-        </Modal>
-    </div>
+    return (
+        <div className='imgCarrito'>
+            <Button onClick={handleOpen}>
+                <ShoppingCartIcon className="iconoCarrito" fontSize="large"/>
+            </Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Modal de prueba
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Acá irian objetos
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
     );
 }
