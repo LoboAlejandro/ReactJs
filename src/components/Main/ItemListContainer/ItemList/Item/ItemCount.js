008fe310e0a568890e36622a60b3806cf07c167e
alejandro lobo <alejandro.lobo_97@hotmail.com>
2022-03-30 03:21:44 -0300

    correccion clase 02

--- src/components/Main/ItemListContainer/ItemList/Item/ItemCount.js
+++ src/components/Main/ItemListContainer/ItemList/Item/ItemCount.js
@@ -1,9 +1,18 @@
 import React, { useEffect } from 'react';
 
-export default function ItemCount({count, id}){
+export default function ItemCount({count}){
     const aviso = () =>{
         alert(`Usted ha agregado ${count} producto/s`)
     }
+
+    useEffect(()=>{
+        let cambioCount= document.querySelector('.btnAgregar');
+        if(count === 0){
+            cambioCount.disabled= true;
+        }else{
+            cambioCount.disabled= false;
+        }
+    }, [count])
     
     return(
         <button className='btnAgregar' onClick={aviso}>Agregar al Carrito</button>
