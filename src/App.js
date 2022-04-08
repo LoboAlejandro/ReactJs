f104d15157b75ba7d8c1156d87e55582337c032b
alejandro lobo <alejandro.lobo_97@hotmail.com>
2022-04-08 04:26:29 -0300

    CorrecciÃones y agregado de categorias

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path='/' element={<HomePage/>}/>
-          <Route path='/detalle' element={<DetailPage/>}/>
           <Route path='/productos' element={<InConstructionPage/>}/>
           <Route path='/nosotros' element={<InConstructionPage/>}/>
           <Route path='/contacto' element={<InConstructionPage/>}/>
           <Route path='/producto/:id' element={<DetailPage/>}/>
+          <Route path="/:category" element={<HomePage/>}/>
           <Route path='/cart' element={<CartPage/>}/>
           <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
--- src/components/Main/ItemList.js
+++ src/components/Main/ItemList.js
@@ -1,21 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import { dataProducts } from '../../data/data';
 import { Item } from './Item';
+import { useParams } from 'react-router-dom';
     
 export default function ItemList(){
     
+    const { category } = useParams();
+    
+    let prueba= 'CARGANDO ITEM';
     const [products, setProducts]= useState([]);
     
     const getProducts= new Promise((resolve, reject)=>{
-            return setTimeout(()=>{
-                return resolve(dataProducts);
-            }, 2000)
+        return setTimeout(()=>{
+            return resolve(dataProducts);
+        }, 2000)
     })
-
+    
     async function getProductsAsync(){
         try{
             const productos= await getProducts;
-            setProducts(productos);
+            setProducts([]);
+            category ? filterProductByCategory(productos, category) : setProducts(productos);
         }catch(error){
             console.log('error', error);
         }
@@ -23,7 +28,32 @@ export default function ItemList(){
     
     useEffect(()=>{
         getProductsAsync();
-    },[])
+    }, [category])
+    
+    const filterProductByCategory = (productos , category) => {
+        prueba= 'error';
+        productos.map( (producto) => {
+            if(producto.category === category) {
+                setProducts(products => [...products, producto]);
+                prueba= 'Correcto';
+            }
+        })
+    }
+    
+    useEffect(()=>{
+        const msjError= document.querySelector('.msjError');
+        const msjCargando= document.querySelector('.msjCargando');
+        msjError.style.visibility= 'hidden';
+        setTimeout(()=>{
+            if(prueba === 'error'){
+                console.log('error');
+                msjCargando.style.visibility= 'hidden';
+                msjError.style.visibility= 'visible';
+            }else{
+                console.log('correcto');
+            }
+        }, 2000)
+    }, [prueba])
     
     return(
         <div className='itemList'>
@@ -38,7 +68,10 @@ export default function ItemList(){
                     })
                 }</>
             ) : (
-                <h2>Cargando productos...</h2>
+                <div className='divCargandoError'>
+                    <h2 className='msjCargando'>Cargando productos...</h2>
+                    <h2 className='msjError'>ERROR: La categoria ingresada no existe</h2>
+                </div>
             )}
         </div>
     );
